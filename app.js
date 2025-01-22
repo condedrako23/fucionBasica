76f2b7f2aa13229dbb5c9356bd1c8f3013ac5c28
condedrako23 <simonscheffer00@gmail.com>
2025-01-22 00:21:57 -0300

    agregacion de if a la funcion

--- app.js
+++ app.js
@@ -39,6 +39,16 @@ function displayMenssage(msgText, msgType) {
 
     panel.parentNode.removeChild(panel)
     /* especfica que queremos eliminar un elemnto secundario del elemnto html en este caso el panel (div)*/
+
+    if (msgType === "warning") {
+      msg.style.backgroundImage = "url(icons/warning.png)";
+      panel.style.backgroundColor = "red";
+    } else if (msgType === "chat") {
+      msg.style.backgroundImage = "url(icons/chat.png)";
+      panel.style.backgroundColor = "aqua";
+    } else {
+      msg.styale.paddingLeft = "20";
+    }
   }
 }
 let btn = document.querySelector("button")
@@ -46,5 +56,5 @@ let btn = document.querySelector("button")
 //btn.onclick = displayMenssage
 /*en lugar de llamar a una funcion anonima que contiene algun codigo, estamos llamando directamente a nuestra funcion al hacer click en el boton */
 btn.onclick = function () {
-  displayMenssage("wow, es un mensaje diferente")
+  displayMenssage("your inbox is almost full - delet some mails", "warning")
 }
